7332a6cf4991c2cf550be2ceae6efa7f8248146a
mugi88 <m211097268+01@gmail.com>
2020-04-13 11:09:47 +0900

    add users

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,2 +1,54 @@
-class UsersController < ApplicationController
+module V1
+    class UsersController < ApplicationController
+        before_action :set_user, only: [:show, :update, :destroy]
+        #skip_before_action :verify_authenticity_token
+
+        def index
+          users = User.order(created_at: :desc)
+          render json: { status: 'SUCCESS', message: 'Loaded users', data: users }
+        end
+  
+        def show
+          render json: { status: 'SUCCESS', message: 'Loaded the user', data: @user }
+        end
+  
+        def create
+          user = User.new(user_params)
+          if user.save
+            render json: { status: 'SUCCESS', data: user }
+          else
+            render json: { status: 'ERROR', data: user.errors }
+          end
+        end
+  
+        def destroy
+          @user.destroy
+          render json: { status: 'SUCCESS', message: 'Deleted the user', data: @user }
+        end
+  
+        def update
+          if @user.update(user_params)
+            render json: { status: 'SUCCESS', message: 'Updated the user', data: @user }
+          else
+            render json: { status: 'SUCCESS', message: 'Not updated', data: @user.errors }
+          end
+        end
+  
+        private
+  
+        def set_user
+          @user = User.find(params[:id])
+        end
+  
+        def user_params
+          params.require(:user).permit(:title)
+        end
+    end
 end
+###        def user_params
+###          params.require(:user).permit(
+###            :name, :display_name, :email, :password,
+###            :confirmation-password, :organization, :boss
+###          )
+###        end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
-end
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  
+    has_secure_token
+  
+    # これUpdate時以外も効くように変更する
+    # PasswordのValidationsをUpdate時のみ実行
+    has_secure_password validations: false
+    validates :password, on: :update, confirmation: { allow_blank: true },
+                         length: { maximum: ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED }
+    validates :password_digest, on: :update, presence: true
+    validates :name, length: { in: 2..20 }, allow_nil: true
+    validates :email, on: :update, presence: true, length: { maximum: 255 },
+                      uniqueness: { case_sensitive: false },
+                      format: { with: VALID_EMAIL_REGEX }
+    validates :birth_year, numericality: { in: 1900..Time.zone.today.year }
+  
+    before_save { self.email = email&.downcase }
+  end
+  
